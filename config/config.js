4e2e2887f946180d22791cf8c4f4328a7e55d850
raej1428 <rachaelgladwin@Rachaels-iMac.local>
2020-06-01 20:49:21 -0600

    fixing jaws

--- config/config.js
+++ config/config.js
@@ -6,18 +6,18 @@ module.exports = {
         password: process.env.password,
         database: process.env.database,
         host: process.env.host,
-        dialect: 'mysql'
+        dialect: 'mysql2'
     },
     test: {
         username: process.env.user,
         password: process.env.password,
         database: process.env.database,
         host: process.env.host,
-        dialect: 'mysql'
+        dialect: 'mysql2'
     },
     production: {
         use_env_variable: process.env.JAWSDB_URL,
-        dialect: 'mysql'
+        dialect: 'mysql2'
     }
     
 }
