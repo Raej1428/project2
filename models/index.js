37daba543fe7399b0733e8656fd0428c77b0c00b
raej1428 <rachaelgladwin@Rachaels-iMac.local>
2020-06-01 23:57:07 -0600

    fixing jaws and heroku errors

--- models/index.js
+++ models/index.js
@@ -1,37 +1,34 @@
-"use strict";
+'use strict';
 
-require("dotenv").config();
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+const db = {};
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var connection = require(__dirname + "/../config/config.js")[env];
-var db = {};
-
-if (connection.use_env_variable) {
-    var sequelize = new Sequelize(process.env[connection.use_env_variable]);
+let sequelize;
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    var sequelize = new Sequelize('recipes_app_db', 'root', 'password', {
+    sequelize = new Sequelize(config.database, config.username, config.password, {
         host: 'localhost',
         dialect: 'mysql'
-    });
+    }, config);
 }
 
-
-fs.readdirSync(__dirname)
-    .filter(function(file) {
-        return (
-            file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-        );
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
-    .forEach(function(file) {
-        var model = sequelize.import(path.join(__dirname, file));
+    .forEach(file => {
+        const model = sequelize['import'](path.join(__dirname, file));
         db[model.name] = model;
     });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach(modelName => {
     if (db[modelName].associate) {
         db[modelName].associate(db);
     }
---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+module.exports = {
+    development: {
+        username: process.env.user,
+        password: process.env.password,
+        database: process.env.database,
+        host: process.env.host,
+        dialect: 'mysql'
+    },
+    test: {
+        username: process.env.user,
+        password: process.env.password,
+        database: process.env.database,
+        host: process.env.host,
+        dialect: 'mysql'
+    },
+    production: {
+        use_env_variable: process.env.JAWSDB_URL,
+        dialect: 'mysql'
+    }
+    
+}
